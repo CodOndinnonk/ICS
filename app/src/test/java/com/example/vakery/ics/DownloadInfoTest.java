2f0acf9183826566c0396c921a63f2588ba05f28
CodemanForever <val0995@mail.ru>
2017-06-04 15:58:20 +0300

    add few tests

--- app/src/test/java/com/example/vakery/ics/DownloadInfoTest.java
+++ app/src/test/java/com/example/vakery/ics/DownloadInfoTest.java
@@ -1,8 +1,30 @@
 package com.example.vakery.ics;
 
+import android.content.Context;
+
+import com.example.vakery.ics.Application.Functional.DownloadInfo;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
+
 import static org.mockito.Mockito.*;
 
 
 public class DownloadInfoTest {
-    
+
+    DownloadInfo mockDownloadInfo  = mock(DownloadInfo.class);
+    Context mockContext = mock(Context.class);
+
+    @Test
+    public void testMethodsCall(){
+        mockDownloadInfo.updateInfo();
+        mockDownloadInfo.checkForInformation();
+        when(mockDownloadInfo.checkInternetConnection(mockContext)).thenReturn(true);
+
+        verify(mockDownloadInfo).updateInfo();
+        verify(mockDownloadInfo).checkForInformation();
+        Mockito.doReturn(true).when(mockDownloadInfo).checkInternetConnection(mockContext);
+    }
+
 }
---
+++ app/src/test/java/com/example/vakery/ics/MyToolbarTest.java
@@ -0,0 +1,30 @@
+package com.example.vakery.ics;
+
+
+import android.content.Context;
+
+import com.example.vakery.ics.Application.Functional.DownloadInfo;
+import com.example.vakery.ics.Application.Functional.MyToolbar;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class MyToolbarTest {
+
+    MyToolbar mock  = mock(MyToolbar.class);
+
+
+    @Test
+    public void testMethodsCall(){
+        mock.prepareForExit();
+        mock.createToolbar("123");
+
+        verify(mock,atLeastOnce()).prepareForExit();
+        verify(mock).createToolbar("123");
+    }
+}
